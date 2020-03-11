86e38759a98553cea945f569c6c2ea4871cc92b8
mixer <playrexim@gmail.com>
2020-03-11 10:40:38 +0600

    registrate income, cost

--- apps/econom/admin.py
+++ apps/econom/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Section, Wallet, CostCategory, IncomeCategory
+from .models import (
+    Section,
+    Wallet,
+    CostCategory,
+    IncomeCategory,
+    Cost,
+    Income,
+)
 
 # Register your models here.
 
@@ -7,3 +14,5 @@ admin.site.register(Section)
 admin.site.register(Wallet)
 admin.site.register(CostCategory)
 admin.site.register(IncomeCategory)
+admin.site.register(Cost)
+admin.site.register(Income)

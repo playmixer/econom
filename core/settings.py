3a82df481807687ee40cacfbc4d613c76b129177
Nikita <nikitashkirya@gmail.com>
2020-04-08 22:35:38 +0600

    move django secret key in .end

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", '&ji%8vc(t&3%2y)bp!(9+xv^my3$#(+n3mz5n5%ewbjh-$6w&)')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 

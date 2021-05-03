e40d5ff9d5b537175771e9b9661dda9fda88d808
shkirya.ns <shkirya.ns@oilpc.ru>
2021-05-03 22:47:17 +0600

    command createuser

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from core.database import db
 from main import create_app
+from apps.auth.models import User
+from apps.auth.auth import Auth
 
 app = create_app()
 
@@ -9,6 +11,17 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 migrate = Migrate(app, db)
+
+@manager.command
+def createuser(username, password):
+    user = Auth.registration(username, password)
+    if user:
+        print(f'User {user.username} created')
+        return
+
+    print('User create failed')
+    return
+
 
 if __name__ == "__main__":
     manager.run()

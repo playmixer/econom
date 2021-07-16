22b5d2130ce2370c57645a3ba7866fea13960b18
shkirya.ns <shkirya.ns@oilpc.ru>
2021-07-16 17:38:24 +0600

    fix frontent

--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template
+import config
 
 frontend = Blueprint(
     'frontend',
     __name__,
     template_folder='templates',
-    static_folder='static'
+    static_folder='static',
+    static_url_path=config.SUBDIRECTORY + '/frontend/static'
 )
 
 
